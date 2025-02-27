93ffcdf9af80f0c7c9e6199b63ace26ce73b120f
Elise Green <gelise1520@gmail.com>
2025-02-26 18:37:42 -0600

    add search componet

--- forager/app/page.js
+++ forager/app/page.js
@@ -2,9 +2,11 @@ import DashboardPage from './dashboard/page';  // Use relative path
 import Sandbox from './sandbox/page';  // Use relative path
 import SandboxCopyPage from './sandboxcopy/page';  // Use relative path
 import SandboxCopyCopyPage from './sandboxcopycopy/page';
+import SandboxCopyCubedPage from './sandboxcopy3/page';
 export default function Home() {
   //return <DashboardPage />;  // Render the DashboardPage as the default for the '/' route
   //return <Sandbox />;  // Render the DashboardPage as the default for the '/' route
-  return <SandboxCopyPage />;  // Render the Sandbox as the default for the '/' route
+ // return <SandboxCopyPage />;  // Render the Sandbox as the default for the '/' route
   // return <SandboxCopyCopyPage />;  // Render the Sandbox as the default for the '/' route
+  return <SandboxCopyCubedPage />
 }
---
+++ forager/app/sandboxcopy3/page.js
@@ -0,0 +1,13 @@
+import Message from "../../components/Message";
+import { DataMushroomListPercent } from "@/components/MushroomList";
+import {mushroomslistpercent,pill,shroom} from '../../data/development'
+import FilterSettings from '@/components/FilterSettings'
+import Search from "@/components/Search";
+
+import Mushroom from '@/components/Mushroom'
+export default function SandboxCopyCubedPage() {
+    return (
+        <Search />
+    );
+}
+  
